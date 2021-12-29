2d9d5cb0473e52f558647098353c0efafd0aea9a
Markus Lindberg <markus.lindberg89@gmail.com>
2021-12-29 19:45:05 +0100

    added function for finding .gpg files in subdirs

--- gpg_transfer.pl
+++ gpg_transfer.pl
@@ -3,7 +3,10 @@
 use strict;
 use warnings;
 use v5.10;
+use Data::Dumper;
+
 use File::Basename qw(basename dirname);
+use File::Find     qw(find);
 
 my $usage = << 'EOF';
 
@@ -13,38 +16,58 @@ my $usage = << 'EOF';
      -h, --help        Show this help and exit
      -f, --to-key      GPG key to encrypt to
      -d, --dest-dir    Directory to put newly encrypted .gpg files in
+     -s, --source-dir  Directory search for .gpg files
 
 EOF
 
 my $GPG='/usr/bin/gpg';
-my %PARAMS = ( to_key   => '',
-               to_dir   => '.');
+my %PARAMS = ( to_key   => '', to_dir   => '.', from_dir => '');
 
-test();
+main();
 
 #### SUBROUTINES #####
 
-sub test {
-  my $posargs = parse_args();
+sub main {
+  my $files = parse_args();
   my $infile;
-  foreach $infile (@$posargs) {
-    my $decode = gpg_decode($infile);
-    my $encode = gpg_encode($PARAMS{to_key});
-
+  my $encrypt = gpg_encrypt($PARAMS{to_key});
+  find_gpg_files($PARAMS{from_dir}, $files) if ($PARAMS{from_dir});
+  foreach $infile (@{$files}) {
+    my $decrypt = gpg_decrypt($infile);
     my $outfile = "$PARAMS{to_dir}/" . basename($infile);
-    my $cmd =  "$decode | $encode > $outfile";
+    my $cmd =  "$decrypt | $encrypt > $outfile";
     say $cmd;
-    `$cmd`;
+    #`$cmd`;
   }
 }
 
-sub gpg_decode {
+sub find_gpg_files {
+  my $dir     = shift;
+  my $matches = shift;
+  my $depth   = shift || 5;
+
+  if ($depth <= 0) { return -1 }
+  unless (-d $dir) { return 0 }
+  
+  opendir(DFH, $dir);
+  my @files = readdir(DFH);
+  closedir(DFH);
+  foreach my $file (@files) {
+    my $fpath = "$dir/$file";
+    if (-f $fpath && $file =~ /gpg$/)   { push @{$matches}, $fpath }
+    if ($file eq '.' || $file eq '..')  { next }
+    if (-d $fpath )                     { find_gpg_files($fpath, $matches, $depth -1) }
+  }
+  return 1;
+}
+
+sub gpg_decrypt {
   my $infile = shift;
   if (! -f $infile) { die "$usage\n\nFile does not exist: $infile" }
   return "$GPG --decrypt $infile";
 }
 
-sub gpg_encode {
+sub gpg_encrypt {
   my $gpg_id = shift;
   if     ($gpg_id =~ /![\da-f]/i) { die "${usage}Not a valid gpg id: $gpg_id" }
   unless ($gpg_id =~ /[\da-f]/i ) { die "${usage}Not a valid gpg id: $gpg_id" }
