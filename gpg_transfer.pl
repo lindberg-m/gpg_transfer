ab2dec442f8c8b894c876ce8ce18a3a41c27b3e1
Markus Lindberg <markus.lindberg89@gmail.com>
2021-12-29 22:01:08 +0100

    stuff

--- gpg_transfer.pl
+++ gpg_transfer.pl
@@ -8,20 +8,23 @@ use Data::Dumper;
 use File::Basename qw(basename dirname);
 use File::Find     qw(find);
 
-my $usage = << 'EOF';
+my $usage = << "EOF";
 
- usage: $0 [params] file
+ usage: $0 [params] [file]
 
    Optional params:
      -h, --help        Show this help and exit
      -f, --to-key      GPG key to encrypt to
      -d, --dest-dir    Directory to put newly encrypted .gpg files in
      -s, --source-dir  Directory search for .gpg files
+     --dry-run         Only print commands, don't run
+     --force           Transfer file even if target already exist
 
 EOF
 
 my $GPG='/usr/bin/gpg';
-my %PARAMS = ( to_key   => '', to_dir   => '.', from_dir => '');
+my %PARAMS = ( to_key   => '', to_dir   => '.', from_dir => '',
+               dry_run => 0, force => 0);
 
 main();
 
@@ -30,15 +33,31 @@ main();
 sub main {
   my $files = parse_args();
   my $infile;
-  my $encrypt = gpg_encrypt($PARAMS{to_key});
+  my $encrypt_cmd = gpg_encrypt($PARAMS{to_key});
   find_gpg_files($PARAMS{from_dir}, $files) if ($PARAMS{from_dir});
+  my $from_dir_nchar = length($PARAMS{from_dir});
   foreach $infile (@{$files}) {
-    my $decrypt = gpg_decrypt($infile);
-    my $outfile = "$PARAMS{to_dir}/" . basename($infile);
-    my $cmd =  "$decrypt | $encrypt > $outfile";
-    say $cmd;
-    #`$cmd`;
+    my $decrypt_cmd = gpg_decrypt($infile);
+    my $outfile = "$PARAMS{to_dir}/" . substr($infile, $from_dir_nchar); $outfile =~ s://+:/:g;
+    if (outfile_is_ok($outfile)) {
+      my $cmd =  "$decrypt_cmd | $encrypt_cmd > $outfile";
+      say $cmd;
+      #`$cmd`;
+    } else {
+      say STDERR "File $outfile already exist, will not transfer $infile to $outfile (use --force to override)";
+    }
   }
+}
+
+sub outfile_is_ok {
+  my $fpath = shift;
+  my $force = shift || 0;
+  
+  if (-f $fpath) { return $force ? 1 : 0; }
+
+  my $dname = dirname($fpath);
+  mkdir $dname unless (-d $dname);
+  return 1;
 }
 
 sub find_gpg_files {
@@ -47,14 +66,14 @@ sub find_gpg_files {
   my $depth   = shift || 5;
 
   if ($depth <= 0) { return -1 }
-  unless (-d $dir) { return 0 }
+  unless (-d $dir) { return 0  }
   
   opendir(DFH, $dir);
   my @files = readdir(DFH);
   closedir(DFH);
   foreach my $file (@files) {
     my $fpath = "$dir/$file";
-    if (-f $fpath && $file =~ /gpg$/)   { push @{$matches}, $fpath }
+    if (-f $fpath && $file =~ /\.gpg$/) { push @{$matches}, $fpath }
     if ($file eq '.' || $file eq '..')  { next }
     if (-d $fpath )                     { find_gpg_files($fpath, $matches, $depth -1) }
   }
@@ -84,6 +103,8 @@ sub parse_args {
       if ($arg eq '-t' || $arg eq '--to-key')     { $PARAMS{to_key}   = $ARGV[++$i] }
       if ($arg eq '-s' || $arg eq '--source-dir') { $PARAMS{from_dir} = $ARGV[++$i] }
       if ($arg eq '-d' || $arg eq '--dest-dir')   { $PARAMS{to_dir}   = $ARGV[++$i] }
+      if ($arg eq '--dry-run')                    { $PARAMS{dry_run}  = $ARGV[++$i] }
+      if ($arg eq '--force')                      { $PARAMS{force}  = $ARGV[++$i] }
     } else {
       push @posargs, $arg;
     }
